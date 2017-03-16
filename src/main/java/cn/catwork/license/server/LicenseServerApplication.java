ab3d6f70d45aef97d2ceec53fccf486dfd9c79eb
yangshinan <yangsn@figo.cn>
2017-03-16 19:46:08 +0800

    导入spring-boot骨架

---
+++ src/main/java/cn/catwork/license/server/LicenseServerApplication.java
@@ -0,0 +1,12 @@
+package cn.catwork.license.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServerApplication.class, args);
+	}
+}
